1eab432e036c65f5dca72949475452a11f34d801
Shreyash Shenoy <shreyash.shenoy@focalworks.in>
2023-02-24 11:03:53 +0530

    Adding the changes to Capture webhook

--- web/app/Lib/Handlers/ProductUpdated.php
+++ web/app/Lib/Handlers/ProductUpdated.php
@@ -6,14 +6,24 @@ namespace App\Lib\Handlers;
 
 use Illuminate\Support\Facades\Log;
 use Shopify\Webhooks\Handler;
+use App\Models\Watchlist;
+use App\Http\Controllers\WatchListController;
 
 class ProductUpdated implements Handler
 {
     public function handle(string $topic, string $shop, array $body): void
-    {
+    {   
         Log::debug(
             "Product updated from the shop $shop with response body: " .
-                print_r($body, true)
-        );
+                print_r($body['id'], true)
+            );
+        $productId = $body['id'];
+        $productUsers = $this->getCustomer($productId,$shop);
+        Log::debug($productUsers);
+    }
+
+    public function getCustomer($productId,$shop){
+        $productsUsers =  Watchlist::where('shop_id', $shop)->where('product_id', $productId)->get();
+        return $productsUsers;
     }
 }
